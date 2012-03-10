fccb26eebba1f1994ed14ad7f3fdffd5bc24629a
niobium93 <niobium93@gmail.com>
2012-03-10 21:02:05 +0200

    First commit. Got current user's startup programs to show up in a listview.
    Signed-off-by: niobium93 <niobium93@gmail.com>

---
+++ Startup Manager/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Startup_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            RegistryKey userRunKey = Registry.CurrentUser.OpenSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run");
+
+            string[] subKeys = userRunKey.GetValueNames();
+            foreach (string key in subKeys)
+            {
+                CreateRow(key, userRunKey.GetValue(key).ToString());
+            }
+
+            userRunKey.Close();
+        }
+        private void CreateRow(string name, string location)
+        {
+            ListViewItem item = new ListViewItem(name);
+            item.SubItems.Add(location);
+            listView1.Items.Add(item);
+        }
+    }
+}
